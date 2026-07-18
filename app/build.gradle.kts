b51a33466a037e6bb6a33e2af1066ded46b043fd
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-07-18 23:08:22 +0300

    update SDK to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 android {
     namespace = "com.nicos.pitchkit"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
@@ -35,6 +33,7 @@ android {
     buildFeatures {
         compose = true
     }
+    buildToolsVersion = "37.0.0"
 }
 
 dependencies {
