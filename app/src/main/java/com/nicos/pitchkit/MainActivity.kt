4ca72e09775ee369edf109975ef5e9b24f7bc61a
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-07-18 22:59:11 +0300

    initial commit

---
+++ app/src/main/java/com/nicos/pitchkit/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.nicos.pitchkit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nicos.pitchkit.ui.theme.PitchKitTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PitchKitTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
