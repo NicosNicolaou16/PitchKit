0e04240060fae732c1fdd6e833a8e995435153ea
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-08-13 21:33:35 +0300

    setup a library module

--- PitchKit/build.gradle.kts
+++ PitchKit/build.gradle.kts
@@ -21,9 +21,20 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.compose.runtime)
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
-    implementation(libs.material)
+    // Coroutines
+    implementation(libs.coroutine.core)
+    implementation(libs.coroutine.android)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
