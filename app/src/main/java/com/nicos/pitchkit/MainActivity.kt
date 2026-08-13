7f9fde8c2f02962a5ca7948fbf389ca9acd0040a
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-08-13 21:10:30 +0300

    setup a library module

--- app/src/main/java/com/nicos/pitchkit/MainActivity.kt
+++ app/src/main/java/com/nicos/pitchkit/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.nicos.pitchkit
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,8 +8,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,7 +15,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.nicos.pitchkit.tuner.GuitarTunerListener
-import com.nicos.pitchkit.tuner.TunerEngine
 import com.nicos.pitchkit.ui.theme.PitchKitTheme
 
 class MainActivity : ComponentActivity() {
