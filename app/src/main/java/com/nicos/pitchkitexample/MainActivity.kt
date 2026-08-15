c60e66298af5557d19bd88d3b6c823606eb71525
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-08-15 18:24:59 +0300

    create helper methods for guitar

--- app/src/main/java/com/nicos/pitchkitexample/MainActivity.kt
+++ app/src/main/java/com/nicos/pitchkitexample/MainActivity.kt
@@ -7,10 +7,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -24,8 +40,6 @@ import com.nicos.pitchkit.tuner.GuitarTunerListener
 import com.nicos.pitchkit.tuner.TuningResult
 import com.nicos.pitchkit.tuner.guitar_helpers.isPegActive
 import com.nicos.pitchkitexample.ui.theme.PitchKitTheme
-import kotlin.math.abs
-import kotlin.math.ln
 
 class MainActivity : ComponentActivity() {
 
